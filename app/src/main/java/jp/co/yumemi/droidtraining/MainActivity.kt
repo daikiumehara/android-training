15f3796e16659e9931fdc87dee6b0eee3dbc9d29
daikiumehara <daikiumehara3@gmail.com>
2023-10-17 15:23:42 +0900

    edit: use weather api

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -13,10 +13,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,17 +31,18 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent(maxTemp = 10, minTemp = 5)
+            MainContent()
         }
     }
 }
 
 @Composable
-fun MainContent(maxTemp: Int, minTemp: Int) {
+fun MainContent() {
+    val weatherClient = WeatherAPIClientImpl(context = LocalContext.current)
+    var weather: WeatherCase by remember { mutableStateOf(WeatherCase.SUNNY) }
         ConstraintLayout(
             constraintSet = mainViewConstraints(),
             modifier = Modifier.fillMaxSize()
@@ -47,68 +53,91 @@ fun MainContent(maxTemp: Int, minTemp: Int) {
                     .fillMaxWidth(0.5f)
                     .layoutId("weatherView")
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = null,
-                    Modifier
-                        .aspectRatio(1f)
-                )
-                Row(
-                    modifier = Modifier.fillMaxWidth(1f)
-                ) {
-                    Text(
-                        maxTemp.toString(),
-                        color = Color.Red,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .weight(1f)
-                    )
+                WeatherImage(weather.value)
 
-                    Text(
-                        minTemp.toString(),
-                        color = Color.Blue,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .weight(1f)
-                    )
-                }
+                TempLabels(maxTemp = 10, minTemp = 5)
             }
 
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .layoutId("actionButtons")
-            ) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.weight(1f)
-
-                ) {
-                    Button(
-                        onClick = { /*TODO*/ },
-                    ) {
-                        Text(
-                            "RELOAD",
-                            fontSize = 13.sp
-                        )
-                    }
-                }
-
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.weight(1f)
-                ) {
-                    Button(
-                        onClick = { /*TODO*/ },
-                    ) {
-                        Text(
-                            "NEXT"
-                        )
-                    }
-                }
-            }
+            ActionButtons(
+                reloadAction = {
+                    weather = weatherClient.fetchWeather()
+                },
+                nextAction = {}
+            )
         }
 }
+
+@Composable
+fun WeatherImage(id: Int) {
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = null,
+        Modifier
+            .aspectRatio(1f)
+    )
+}
+
+@Composable
+fun TempLabels(maxTemp: Int, minTemp: Int) {
+    Row(
+        modifier = Modifier.fillMaxWidth(1f)
+    ) {
+        Text(
+            maxTemp.toString(),
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .weight(1f)
+        )
+
+        Text(
+            minTemp.toString(),
+            color = Color.Blue,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .weight(1f)
+        )
+    }
+}
+
+
+@Composable
+fun ActionButtons(reloadAction: () -> Unit, nextAction: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .layoutId("actionButtons")
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.weight(1f)
+
+        ) {
+            Button(
+                onClick = reloadAction,
+            ) {
+                Text(
+                    "RELOAD",
+                    fontSize = 13.sp
+                )
+            }
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.weight(1f)
+        ) {
+            Button(
+                onClick = nextAction,
+            ) {
+                Text(
+                    "NEXT"
+                )
+            }
+        }
+    }
+}
+
 
 fun mainViewConstraints(): ConstraintSet {
     return ConstraintSet {
@@ -130,5 +159,5 @@ fun mainViewConstraints(): ConstraintSet {
 @Preview
 @Composable
 fun MainContent_Preview() {
-    MainContent(maxTemp = 10, minTemp = 3)
+    MainContent()
 }
