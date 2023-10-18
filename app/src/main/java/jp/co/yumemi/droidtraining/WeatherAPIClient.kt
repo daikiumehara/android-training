95b9dfe733bddbaadcdf78fef15e4b40d8d07e28
daikiumehara <daikiumehara3@gmail.com>
2023-10-18 14:49:28 +0900

    edit: use throws method

--- app/src/main/java/jp/co/yumemi/droidtraining/WeatherAPIClient.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/WeatherAPIClient.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import jp.co.yumemi.api.YumemiWeather
 
 interface WeatherAPIClient {
-    fun fetchWeather(): WeatherCase
+    fun fetchWeather(): Pair<WeatherInfo?, String?>
 }
 
 class WeatherAPIClientImpl(context: Context): WeatherAPIClient {
     private val api: YumemiWeather = YumemiWeather(context = context)
-    override  fun fetchWeather(): WeatherCase {
-        return WeatherCaseConvertor.convert(api.fetchSimpleWeather())
+    override  fun fetchWeather(): Pair<WeatherInfo?, String?> {
+        return try {
+            val weatherCase = WeatherCaseConvertor.convert(api.fetchThrowsWeather())
+            WeatherInfo(weatherCase, 10, 0) to null
+        } catch(e: Throwable) {
+            null to "エラーが発生しました"
+        }
     }
 }
